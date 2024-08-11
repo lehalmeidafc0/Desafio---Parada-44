96438a595cefa1d6295e2ac55c0a174a3de1d65a
lehalmeidafc0 <144854855+lehalmeidafc0@users.noreply.github.com>
2024-08-11 15:44:13 -0300

    Add files via upload

---
+++ exemplo_03.py
@@ -0,0 +1,8 @@
+import time
+
+# Início da contagem regressiva
+for i in range(10, -1, -1):
+    print(i)
+    time.sleep(1)  # Espera 1 segundo antes de continuar a contagem
+
+print("Contagem regressiva finalizada!")
---
+++ exemplo_05.py
@@ -0,0 +1,6 @@
+# Solicita ao usuário que digite uma string
+palavra = input("Digite uma palavra: ")
+
+# Exibe cada letra da palavra digitada
+for letra in palavra:
+    print(letra)
